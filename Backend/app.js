fd57fbe349fe5572cc725df9efe30385c33e8f90
Therese Hagwall <90thejag@skola.boras.se>
2023-05-15 17:49:30 +0200

    Add crypto, cors, mongodb

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = requrie('cors');
+const crypto = require('crypto-js');
+const MongoClient = requrie('mongodb').MongoClient;
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
