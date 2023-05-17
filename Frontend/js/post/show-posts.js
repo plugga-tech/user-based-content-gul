827f431a2723127ec96821259fd318c9b181d873
Felix Anneblad <felix.anneblad@gmail.com>
2023-05-17 15:02:43 +0200

    create btn in js instead

--- Frontend/js/post/show-posts.js
+++ Frontend/js/post/show-posts.js
@@ -3,7 +3,6 @@ fetch('http://localhost:3000/api/posts')
   .then(posts => {
     // Get the container element to display the posts
     const container = document.getElementById('posts');
-    const backButton = document.getElementById('backBtn');
 
     // Display all posts with title
     posts.forEach(post => {
@@ -21,16 +20,17 @@ fetch('http://localhost:3000/api/posts')
       postEl.appendChild(authorDateEl);
       container.appendChild(postEl);
 
+      // Add click event listener to each title element
       titleEl.addEventListener('click', () => {
         // Hide all posts
         container.style.display = 'none';
-        backButton.style.display = 'block';
 
         // Create a container for the selected post
         const selectedPostContainer = document.createElement('div');
         const selectedTitleEl = document.createElement('h2');
         const selectedContentEl = document.createElement('p');
         const selectedAuthorDateEl = document.createElement('p');
+        const backButton = document.createElement('button');
 
         selectedTitleEl.textContent = post.title;
         selectedContentEl.textContent = post.content;
@@ -38,12 +38,18 @@ fetch('http://localhost:3000/api/posts')
         selectedPostContainer.appendChild(selectedTitleEl);
         selectedPostContainer.appendChild(selectedContentEl);
         selectedPostContainer.appendChild(selectedAuthorDateEl);
+        selectedPostContainer.appendChild(backButton);
         document.body.appendChild(selectedPostContainer);
+
+        // Style the back button
+        backButton.textContent = '< Back';
+        backButton.id = 'backBtn';
+        backButton.style.marginTop = '1rem';
 
         // Add click event listener to back button
         backButton.addEventListener('click', () => {
+          // Show all posts and remove the selected post container
           container.style.display = 'block';
-          backButton.style.display = 'none';
           document.body.removeChild(selectedPostContainer);
         });
       });
