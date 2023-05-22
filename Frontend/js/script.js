bf08b35d06c6587769413ac7359775713adcf9a8
sebbehakansson <sebastian.sh74@gmail.comm>
2023-05-22 14:33:11 +0200

    stuff

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,10 +1,12 @@
 'use strict';
-
+import createPost from './post/creatPost.js';
 import printPosts from './post/show-posts.js';
 import headerBtn from './headerBtn.js';
 import printUsers from './printUsers.js';
 if(!localStorage.getItem('user')){
+
     headerBtn();
+    createPost();
 }
 printUsers();
 // printPosts();
