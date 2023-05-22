948056b1479f0a027e7ba82d28b854eb86ebd53b
Tony Hagwall <Hagwall86@live.se>
2023-05-22 16:15:54 +0200

    fix on main sorry!

--- Frontend/js/post/creatPost.js
+++ Frontend/js/post/creatPost.js
@@ -6,7 +6,7 @@ export default async function createPost(title, content, author ) {
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ title: title, content: content, username: author })
+        body: JSON.stringify({ title: title, content: content, userId: author })
     })
 
     let data = await response.json();
